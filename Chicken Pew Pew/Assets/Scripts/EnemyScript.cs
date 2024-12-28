06a8d3b3d2fd3a4217f9f2b00a1c0b0605363349
Pandorexx <kiriku13579@gmail.com>
2024-12-28 19:42:53 +0630

    merge

--- Chicken Pew Pew/Assets/Scripts/EnemyScript.cs
+++ Chicken Pew Pew/Assets/Scripts/EnemyScript.cs
@@ -12,6 +12,15 @@ public class EnemyScript : MonoBehaviour
     [Header("Attack")]
     [SerializeField] int AttackDamage;
     // Start is called before the first frame update
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+
+
+=======
+>>>>>>> main
+>>>>>>> Stashed changes
     void Start()
     {
         CurrentHP = MaxHP;
@@ -43,4 +52,11 @@ public class EnemyScript : MonoBehaviour
     {
         GameObject.Destroy(gameObject);
     }
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+ 
+=======
+>>>>>>> main
+>>>>>>> Stashed changes
 }
