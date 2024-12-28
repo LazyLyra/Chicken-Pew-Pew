81a0d05192b07cec925f345f67d1f8b9419de73d
Pandorexx <kiriku13579@gmail.com>
2024-12-28 19:49:37 +0630

    Reapply "enemyattack"
    This reverts commit 1ac7d8454b57b5218c68bdb9df1d088266b6e5fc.

---
+++ Chicken Pew Pew/Assets/Scripts/EnemyAim.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyAim : MonoBehaviour
+{
+    [SerializeField] PlayerMovementScript playerMovementScript;
+    [SerializeField] FollowEnemyAI followEnemyAI;
+    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject attackRegion;
+    [SerializeField] float coolDownTimer;
+    [SerializeField] float attackTime;
+   
+
+
+
+    private void Update()
+    {
+        Vector3 playerPosition = playerMovementScript.transform.position;
+        Vector2 Direction = playerPosition - enemy.transform.position;
+        if (Direction.magnitude > 0.5f)
+        {
+            transform.up = Direction;
+        }
+        else
+        {
+            if(coolDownTimer<= 0)
+            { 
+                StartCoroutine(visualHandling());
+                coolDownTimer = 1.5f;
+            }
+
+        }
+        coolDownTimer -= Time.deltaTime;
+    }
+    private IEnumerator visualHandling()
+    {
+        attackRegion.SetActive(true);
+        yield return new WaitForSeconds(attackTime);
+        attackRegion.SetActive(false);
+
+    }
+}   
